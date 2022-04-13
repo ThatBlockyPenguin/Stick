c6c2b05476c88bf60ac1dd06ae466b3e644dae12
BlockyPenguin <me@blockypenguin.com>
2022-04-13 17:08:01 +0100

    Fixed file logging

---
+++ README.md
@@ -0,0 +1,63 @@
+# Stick
+Stick is a tiny logger for Deno.
+
+Here's how to use it:
+
+## Logger
+The most basic way to use Stick is to create a default `Logger` instance.
+```js
+import Logger from 'https://deno.land/x/stick/mod.ts';
+
+const logger = new Logger();
+logger.info('Hello, world!');
+// [Thu, 13 Jan 2022 09:18:41 GMT] [INFO] Hello, world!
+```
+The output format is "[label] [name] [level] message". Note that `name` is optional, and left out by default.
+
+`Logger` can also take parameters:
+```js
+Logger(level: Logger.LogLevel, label: string | () => string, name: string, file: string)
+```
+By default, Loggers do not log to a file, however, you can enable this by specifying a file in the `file` parameter.
+
+However, an easier way to create a `Logger` instance is to use the Builder:
+
+## Logger.Builder
+```js
+new Logger.Builder().setName('Process XYZ').build();
+```
+
+`Logger.Builder` has a few functions you can use: `setName`, `setLevel`, `setLabel`, and `setFile`. Once you have set everything you need, call `build` to receive a new `Logger`.
+
+## Logger.LogLevel
+`LogLevel`s are how Stick determines what messages get logged. The current available levels are:
+
+### ERROR
+Use when logging an error. Level 0.
+
+### WARNING
+Use when warning the user. Level 1.
+
+### INFO
+Use when logging general information. Level 2.
+
+### DEBUG
+Use when logging information for debugging purposes. Level 3.
+
+When a `Logger` instance is created, it is assigned a `LogLevel`. By default, this is `INFO`. A Logger will not log any messages above it's specified level - e.g. a Logger on INFO will not log DEBUG messages, but will log INFO, WARNING, and ERROR messages.
+
+## Logger.Labels
+This is an object which contains a few pre-specified labels for creating Loggers with. The current available labels are:
+
+### UTC_DATE_TIME
+Resolves to `new Date().toUTCString()`
+This is the default for all new Loggers
+
+### HH_MM_SS
+Resolves to `new Date().toISOString().substring(11, 19)` (The current time in HH:MM:SS format)
+
+### HH_MM
+Resolves to `new Date().toISOString().substring(11, 16)` (The current time in HH:MM format)
+
+### ISO_STRING
+Resolves to `new Date().toISOString()`
---
+++ mod.ts
@@ -0,0 +1,158 @@
+// deno-lint-ignore-file no-namespace
+import { ensureFileSync } from 'https://deno.land/std@0.134.0/fs/ensure_file.ts';
+import * as colours from "https://deno.land/std@0.134.0/fmt/colors.ts";
+
+type Label = string | (() => string);
+
+class Logger {
+  private readonly label: () => string;
+
+  constructor(public readonly level = Defaults.level, label: Label = Defaults.label, public readonly name?: string, public readonly file?: string) {
+    if (!(label instanceof Function))
+      this.label = () => label;
+    else
+      this.label = label;
+  }
+
+  public log(level: Logger.LogLevel, msg: string): boolean {
+    const logIt = level <= this.level;
+
+    if (logIt) {
+      const log = new TextEncoder().encode(
+        Logger.LogLevel.colourOf(level)(
+          // Label
+          '[' + this.label() + '] ' +
+          // Name (if any)
+          (this.name === undefined ? '' : '[' + this.name + '] ') +
+          // Level
+          '[' + Logger.LogLevel[level] + '] ' +
+          // Message
+          msg + '\n'
+        )
+      );
+
+      // No colouring
+      const strippedLog = new TextEncoder().encode(
+        // Label
+        '[' + this.label() + '] ' +
+        // Name (if any)
+        (this.name === undefined ? '' : '[' + this.name + '] ') +
+        // Level
+        '[' + Logger.LogLevel[level] + '] ' +
+        // Message
+        msg + '\n'
+      );
+
+      Deno.stdout.writeSync(log);
+
+      if (this.file !== undefined) {
+        ensureFileSync(this.file);
+        const file = Deno.openSync(this.file, { create: true, append: true });
+        file.writeSync(strippedLog);
+        file.close();
+      }
+    }
+
+    return logIt;
+  }
+
+  public error(msg: string, throws = false): boolean {
+    const result = this.log(Logger.LogLevel.ERROR, msg);
+
+    if(throws)
+      Deno.exit(-1);
+
+    return result;
+  }
+
+  public warning(msg: string): boolean {
+    return this.log(Logger.LogLevel.WARNING, msg);
+  }
+
+  public info(msg: string): boolean {
+    return this.log(Logger.LogLevel.INFO, msg);
+  }
+
+  public debug(msg: string): boolean {
+    return this.log(Logger.LogLevel.DEBUG, msg);
+  }
+}
+
+namespace Logger {
+  /**
+   * Each level will recieve logs for itself, and every level lower than itself.
+   * 
+   * ERROR < WARNING < INFO < DEBUG
+   */
+  export enum LogLevel {
+    /** Use when logging an error. Level 0 */
+    ERROR,
+    /** Use when warning the user. Level 1 */
+    WARNING,
+    /** Use when logging general information. Level 2 */
+    INFO,
+    /** Use when logging information for debugging purposes. Level 3 */
+    DEBUG
+  }
+
+  export namespace LogLevel {
+    export function colourOf(level: LogLevel) {
+      switch (level) {
+        case LogLevel.ERROR:
+          return colours.brightRed;
+        case LogLevel.WARNING:
+          return colours.yellow;
+        case LogLevel.INFO:
+          return colours.reset;
+        case LogLevel.DEBUG:
+        return colours.brightCyan;
+      }
+    }
+  }
+
+  export const Labels = {
+    UTC_DATE_TIME: () => new Date().toUTCString(),
+    HH_MM_SS: () => new Date().toISOString().substring(11, 19),
+    HH_MM: () => new Date().toISOString().substring(11, 16),
+    ISO_STRING: () => new Date().toISOString()
+  }
+
+  export class Builder {
+    private label: Label = Defaults.label;
+    private level: Logger.LogLevel = Defaults.level;
+    private name: string | undefined = undefined;
+    private file: string | undefined = undefined;
+
+    public setLabel(label: Label): this {
+
+      this.label = label;
+      return this;
+    }
+
+    public setLevel(level: Logger.LogLevel): this {
+      this.level = level;
+      return this;
+    }
+
+    public setName(name: string): this {
+      this.name = name;
+      return this;
+    }
+
+    public setFile(file: string): this {
+      this.file = file;
+      return this;
+    }
+
+    public build(): Logger {
+      return new Logger(this.level, this.label, this.name, this.file);
+    }
+  }
+}
+
+class Defaults {
+  public static label: Label = Logger.Labels.UTC_DATE_TIME;
+  public static level = Logger.LogLevel.INFO;
+}
+
+export default Logger;
